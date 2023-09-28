806d966f375e7e74e30012c922cb878553adc268
gregorio <gregorio.neto@techsolutio.com>
2023-09-27 21:48:54 -0300

    Ajuste no PerfectArrays

--- GFGJava/src/main/java/org/example/PerfectArrays.java
+++ GFGJava/src/main/java/org/example/PerfectArrays.java
@@ -3,22 +3,13 @@ package org.example;
 public class PerfectArrays {
     public static void main(String[] args) {
         int a[] = {20, 4 ,15, 10, 14, 19, 11, 8, 5 ,19, 13, 8, 18, 13, 3 ,12, 8, 16, 19, 11};
-        System.out.print(IsPerfect(a, a.length));
+        System.out.print(IsPerfect(a,20));
     }
 
     public static boolean IsPerfect(int a[], int n) {
-        String reverse = "";
-        String arr = "";
-
-        for (int e:a) {
-            arr += e;
+        for (int i = 0; i < n / 2; i++) {
+            if (a[i] != a[n - 1 - i]) return false;
         }
-
-        for (int i = n - 1; i >= 0 ; i--) {
-            reverse += a[i];
-        }
-
-        if (arr.equals(reverse)) return true;
-        return false;
+        return true;
     }
 }
