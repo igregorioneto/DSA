92cf75139a3607bc403846dbe2c9b9563b15c3e3
gregorio <igregoriodoneto@outlook.com>
2023-12-08 16:14:11 -0300

    Exercicio DSA

---
+++ LeetCodeJava/src/main/java/beginners/RichestCustomerWealth.java
@@ -0,0 +1,20 @@
+package beginners;
+
+public class RichestCustomerWealth {
+    public static void main(String[] args) {
+        int[][] accounts = {{1,5},{7,3},{3,5}};
+        System.out.println(maximumWealth(accounts));
+    }
+
+    static int maximumWealth(int[][] accounts) {
+        int m = accounts.length, max = Integer.MIN_VALUE;
+        for (int i = 0; i < m; i++) {
+            int n = accounts[i].length, sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += accounts[i][j];
+            }
+            if (sum > max) max = sum;
+        }
+        return max;
+    }
+}
---
+++ LeetCodeJava/src/main/java/beginners/RunningSumOf1dArray.java
@@ -0,0 +1,32 @@
+package beginners;
+
+public class RunningSumOf1dArray {
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,4};
+        for (int e:runningSum(nums)) {
+            System.out.print(e + " ");
+        }
+    }
+
+    public static int[] runningSum(int[] nums) {
+        int sums[] = new int[nums.length];
+        sums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int sum = sums[i - 1] + nums[i];
+            sums[i] = sum;
+        }
+        return sums;
+    }
+
+    public static int[] runningSum2(int[] nums) {
+        int sums[] = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < i + 1; j++) {
+                sum += nums[j];
+            }
+            sums[i] = sum;
+        }
+        return sums;
+    }
+}
