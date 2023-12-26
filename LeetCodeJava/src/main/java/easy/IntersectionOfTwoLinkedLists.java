2a015c668bc3ecadf01ac55751ecf3d6f646d4f0
gregorio <igregoriodoneto@outlook.com>
2023-12-26 10:17:45 -0300

    feat: IntersectionOfTwoLinkedLists

---
+++ LeetCodeJava/src/main/java/easy/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,35 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntersectionOfTwoLinkedLists {
+    public static void main(String[] args) {
+        ListNode headA = new ListNode(4);
+        headA.next = new ListNode(1);
+        headA.next.next = new ListNode(8);
+        headA.next.next.next = new ListNode(4);
+        headA.next.next.next.next = new ListNode(5);
+
+        ListNode headB = new ListNode(5);
+        headB.next = new ListNode(6);
+        headB.next.next = new ListNode(1);
+        headB.next.next.next = headA.next.next;
+
+        ListNode r = getIntersectionNode(headA, headB);
+        ListNode.display(r);
+    }
+
+    static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode a = headA, b = headB;
+
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+
+        return a;
+    }
+}
