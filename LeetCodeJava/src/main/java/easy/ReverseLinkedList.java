7939e11c4b0fff6d6684d7ea10aefea4be74a55b
gregorio <igregoriodoneto@outlook.com>
2024-01-03 21:34:08 -0300

    feat: ReverseLinkedList

---
+++ LeetCodeJava/src/main/java/easy/ReverseLinkedList.java
@@ -0,0 +1,30 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        head = reverseList(head);
+        ListNode.display(head);
+    }
+
+    static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode temp = null;
+        while (curr != null) {
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+}
